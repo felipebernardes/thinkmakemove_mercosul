66145eb1e94f0bbee65c04820ab18c7209386bbd
Felipe Bernardes <bernardes.flp@gmail.com>
2015-07-26 14:15:50 -0300

    Revisa calculo e o torna primitivamente funcional

--- plano_b/main.js
+++ plano_b/main.js
@@ -1,19 +1,21 @@
-calculadora = $('#calculator');
-tempo = calculadora.$('#tempo');
-
-habitantes_sp = 12000000; //
-gasto_minuto = 9; //litros
-
-form.submit = function(e) {
-    e.preventDefault;
+ function calcula() {
     
+    tempo = $('#tempo').val();
+    
+    habitantes_sp = 11900000.0;
+    gasto_minuto = 9.0; //litros no chuveiro
+    area_km2_sp = 1523.0;
+    chuva_por_mm_sp = area_km2_sp * 1000000.0;
+    media_historica_mm_dia = 4.0;
+     
     gasto_individual = tempo * gasto_minuto;
     gasto_total = gasto_individual * habitantes_sp;
+     
+    chuva_disponivel_historica_L = chuva_por_mm_sp * media_historica_mm_dia;
+    comparativo = gasto_total/chuva_disponivel_historica_L;
     
-    return gasto_total;
-    
+    console.log(comparativo);
 }
-
 
 
     
